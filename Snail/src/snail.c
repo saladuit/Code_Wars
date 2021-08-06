fc53517e76c55d1d4f763bc8f7419c2132ab540d
Saladin <safoh@student.codam.nl>
2021-08-06 16:08:40 +0200

    tried to fix makefile didn't work

--- Snail/src/snail.c
+++ Snail/src/snail.c
@@ -6,16 +6,16 @@
 /*   By: safoh <safoh@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/08/04 13:57:18 by safoh         #+#    #+#                 */
-/*   Updated: 2021/08/04 23:54:31 by safoh         ########   odam.nl         */
+/*   Updated: 2021/08/06 15:23:02 by safoh         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "../include/snail.h"
 
+// the numbers of rows and cols are passed separately for historical reasons
 // we get
 int *snail(size_t *outsz, const int **mx, size_t rows, size_t cols) {
-  // the numbers of rows and cols are passed separately for historical reasons
   if (!mx || rows || cols)
 	return (0);
   return (int *)(*outsz = 0);
